ecbaafde9a1cbcc1606506941e2fb82977872b5a
Bradley Mackey <bradleymackey98@googlemail.com>
2017-11-11 21:21:53 +0000

    assignment completed, images verified to be correct

---
+++ closing.py
@@ -0,0 +1,13 @@
+import cv2
+import sys
+from dilation import imageDilation
+from erosion import imageErosion
+
+# read the image from the filename in greyscale
+greyscale_image = cv2.imread(sys.argv[1],0)
+# produce the dilated image
+dilated_image = imageDilation(greyscale_image)
+# produce the eroded (closed) image from this
+closed_image = imageErosion(dilated_image)
+# save the resulting file to disk
+cv2.imwrite(sys.argv[2],closed_image)
